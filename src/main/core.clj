cf88b67499adef308ff1d6c4502808884a19c00e
Robert Schardt <robert.schardt@mailbox.org>
2024-08-14 15:37:53 +0200

    Fetch the package_count for each conformed organization name via organization_show Action
    Add transform_org_show_response fn
    Add get_package_counts_of_organizations fn

--- src/main/core.clj
+++ src/main/core.clj
@@ -4,6 +4,32 @@
   )
   (:gen-class))
 
+(defn transform_org_show_response
+  [json_response url]
+  (if (= json_response 0 )
+    0
+    (if (Boolean/valueOf (get json_response "success"))
+      (do
+        ;;(println (format "Success for: %s" url))
+        (let [result (get json_response "result")]
+          (get result "package_count")))
+      (println (format "Fail for: %s" url)))))
+
+(defn get_package_counts_of_organizations
+  [organization_ids]
+  (map
+   (fn [org_id]
+     (if (= org_id 0)
+       org_id
+       (let [ url (format "https://www.govdata.de/ckan/api/3/action/organization_show?id=%s&include_extras=false&include_users=false&include_groups=false&include_tags=false&include_followers=false" org_id) ]
+         ;;(println (format "Get Request for: %s" url))
+         (Thread/sleep 5)
+         (transform_org_show_response
+          (parse-string
+           (slurp url))
+          url))))
+   organization_ids))
+
 (defn transform_org_autocomplete_response
   [json_response url]
   (if (Boolean/valueOf (get json_response "success"))
@@ -63,6 +89,8 @@
   (println)
   (let [department_list (parse_departments)]
     (let [whitespace_replaced_department_list (replace_whitespaces department_list)]
-      (println (get_api_conform_department_names whitespace_replaced_department_list))))
+      (let [conform_department_list (get_api_conform_department_names whitespace_replaced_department_list)]
+        (let [organization_package_counts (get_package_counts_of_organizations conform_department_list)]
+          (println (organization_package_counts))))))
   (println)
   (println (str "GovData-Dashboard has shutdown!")))
