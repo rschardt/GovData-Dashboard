534d58511ef211d21fd82953bcd13760a3d69212
Robert Schardt <robert.schardt@mailbox.org>
2024-08-14 14:30:46 +0200

    Parse & Flatten departments.json -> reason: subordinates key
    Add parse_departments fn for parsing departments.json
    Add get_department_entry fn for concatting inner structure
    Add cheshire dependency for json parsing

--- src/main/core.clj
+++ src/main/core.clj
@@ -1,7 +1,29 @@
 (ns main.core
+  (:require
+    [cheshire.core :refer :all]
+  )
   (:gen-class))
+
+(defn get_department_entry
+  [outer_entry]
+  (if (contains? outer_entry "subordinates")
+    (concat [ (get outer_entry "name") ]
+      (map
+       (fn [inner_entry] (get inner_entry "name"))
+       (get outer_entry "subordinates")))
+     [ (get outer_entry "name") ]))
+
+(defn parse_departments
+  []
+  (mapcat
+   get_department_entry
+   (get
+    (parse-stream
+     (clojure.java.io/reader "departments.json"))
+    "departments")))
 
 (defn -main
   [& args]
   (println (str "GovData-Dashboard has started!"))
+  (println parse_departments)
   (println (str "GovData-Dashboard has shutdown!")))
