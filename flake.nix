6a30375dd0d554861f8c5140ca4bc57073c35f5f
Robert Schardt <robert.schardt@mailbox.org>
2024-08-14 16:08:34 +0200

    Flakify repository, use nix flake as devShell, run, test, build, packaging and deployment tool
    Add deps-lock.json, generated dependencies from deps.edn for nix
    Add flake.nix, described above
    Add flake.lock, state file for the nix flake

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "A flake for GovData-Dashboard";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    clj-nix.url = "github:jlesquembre/clj-nix";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, clj-nix }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      name = "GovData-Dashboard";
+      author = "rschardt";
+    in
+      {
+        packages = rec {
+          default = clj-nix.lib.mkCljApp {
+            pkgs = pkgs;
+            modules = [
+              # Options: https://jlesquembre.github.io/clj-nix/options/
+              {
+                projectSrc = ./.;
+                name = "${author}/${name}";
+                main-ns = "main.core";
+                nativeImage.enable = true;
+              }
+            ];
+          };
+
+          docker-image = pkgs.dockerTools.buildLayeredImage {
+            name = "GovData-Dashboard";
+            tag = "latest";
+            config = {
+              Cmd = "${default}/bin/${name}";
+            };
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            clojure
+          ];
+        };
+      });
+}
